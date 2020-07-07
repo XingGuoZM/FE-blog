98d6cdb6552f573a77fdd609972bb6818027f2be
XingGuoZM <18969176324@163.com>
2020-07-07 09:53:43 +0800

    promise 使用

--- 造轮子/js篇/promise.js
+++ 造轮子/js篇/promise.js
@@ -1,10 +1,9 @@
 
-async function sum(arr,index){
-  if(index===arr.length-1) return arr[arr.length-1]
-  // console.log(index)
-  return sum(arr,index+1)+arr[index]
+new Promise(function(resolve,reject){
+  resolve()
+}).then(res=>{
 
-}
+}).catch(err=>{
 
-console.log(sum([1,2,3,4],0))
+})
 
