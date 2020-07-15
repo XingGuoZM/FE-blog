06469c26ab6754b17b573ac2200804ce3998f33c
XingGuoZM <18969176324@163.com>
2020-07-15 08:04:50 +0800

    淘宝笔试题READEME

--- 职业/淘宝笔试题/index.js
+++ 职业/淘宝笔试题/index.js
@@ -1,7 +1,24 @@
 
-// 淘宝外包笔试题
+// 求阿里数  
 
-
+function aliNumber(number){
+  let ans=[]
+  for(let i=0;i<100;i++){
+    if(squareNumber(squareNumber(squareNumber(i)))===1){
+      ans.push(i)
+    }
+  }
+  console.log(ans)
+  return ans
+}
+// 获取数位,并求平方和
+function squareNumber(number){
+  if( number && typeof number == 'number'){
+    return (number+'').split('').map(item=>item*item).reduce((prev,curr)=>prev+curr)
+  }
+  return false;
+}
+console.log(aliNumber(100))
 
 
 
