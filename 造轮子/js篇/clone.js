7584c0a8959dc514b5bd1a1e36063e967257d3ff
XingGuoZM <18969176324@163.com>
2020-09-11 00:02:46 +0800

    工具函数

---
+++ 造轮子/js篇/clone.js
@@ -0,0 +1,6 @@
+// 深克隆
+let obj={name:'jack'}
+//法一：
+let ans=JSON.parse(JSON.stringify(obj))
+//法二：
+
---
+++ 造轮子/js篇/time.js
@@ -0,0 +1,8 @@
+//获取时间戳
+// time 示例 2020-09-10 09:00:00
+function getTimeStamps(time){
+  return +new Date(time);
+}
+//时间
+
+console.log(getTimeStamps('2020-09-10 09:00:00'))
