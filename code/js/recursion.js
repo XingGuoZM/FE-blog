fcc3d2a31476805d798d75186eebbc190b5b0504
wb-lzm559047 <18969176324@163.com>
2020-11-09 11:27:53 +0800

    数据结构

--- code/js/recursion.js
+++ code/js/recursion.js
@@ -1,3 +1,27 @@
-global.a=123
-module.a=123
-console.log(global,module)
+
+/**
+ * 循环法
+ * @param {*} count 
+ */
+function add1(count) {
+  let ans = 0;
+  for (let i = 0; i <= count; i++) {
+    ans += i;
+  }
+  return ans;
+}
+
+/**
+ * 递归法
+ * @param {*} count 
+ */
+function add2(count) {
+  if (count === 0) return 0;
+  count--;
+  return add2(count) + count + 1
+}
+
+
+// 测试
+let ans = add2(2);
+console.log(ans);
--- 前端数据结构与算法/递归（Recursion）.md
+++ 前端数据结构与算法/递归（Recursion）.md
@@ -4,6 +4,34 @@
 
 ### 示例  
 1. 递归实现从1到100叠加
+```js
 
+/**
+ * 循环法
+ * @param {*} count 
+ */
+function add1(count) {
+  let ans = 0;
+  for (let i = 0; i <= count; i++) {
+    ans += i;
+  }
+  return ans;
+}
+
+/**
+ * 递归法
+ * @param {*} count 
+ */
+function add2(count) {
+  if (count === 0) return 0;
+  count--;
+  return add2(count) + count + 1
+}
+
+
+// 测试
+let ans = add2(2);
+console.log(ans);
+```
 ### 参考
 
