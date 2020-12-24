4adfdac4ca1070509b29070e91bb4913b3f863d2
wb-lzm559047 <18969176324@163.com>
2020-12-24 14:56:26 +0800

    观察者模式

--- 前端设计模式/DesignPattern/Observer.js
+++ 前端设计模式/DesignPattern/Observer.js
@@ -1,6 +1,41 @@
 /**
  * 观察者模式
  */
-function Observer(){
 
+/**
+ * 观察者
+ */
+function Observer(id) {
+  this.id = id;
+  this.update = (obs) => {
+    console.log(obs.id);
+  }
 }
+/**
+ * 被观察者
+ */
+function Observed() {
+  this.observers = [];
+  this.addObserver = (obs) => {
+    this.observers.push(obs);
+  }
+  this.removeObserver = (obs) => {
+    this.observers = this.observers.filter(item=>item.id!==obs.id);
+  }
+  this.notify = (obs) => {
+    this.observers.forEach(item=>item.update(obs));
+  }
+}
+
+// 测试
+const observed = new Observed();
+
+const observer1 = new Observer(1);
+const observer2 = new Observer(2);
+const observer3 = new Observer(3);
+
+observed.addObserver(observer1);
+observed.addObserver(observer2);
+observed.addObserver(observer3);
+
+observed.notify(observer1);
