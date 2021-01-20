b7ca2fed22c6d0f38e81d83d7a02d23b8164034a
wb-lzm559047 <wb-lzm559047@alibaba-inc.com>
2021-01-20 18:38:43 +0800

    前端工程化 lerna

--- 前端工程化/lerna-demo/packages/module-2/lib/module-2.js
+++ 前端工程化/lerna-demo/packages/module-2/lib/module-2.js
@@ -1,10 +1,12 @@
 'use strict';
 const module1 = require('module-1');
 
+// 验证引入的module-1
+module1();
+
 module.exports = module2;
 
 function module2() {
     // TODO
-    console.log(module1);
     console.log('module-2');
 }
