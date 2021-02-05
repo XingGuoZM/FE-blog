15d19bca24543962b2a1dadb8e190554dfda1eed
XingGuoZM <18969176324@163.com>
2021-02-06 00:15:16 +0800

    前端笔记

--- 前端工程化/lerna-react/example/index.js
+++ 前端工程化/lerna-react/example/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Hello } from 'hello-world';
-
+// console.log(Hello)
 ReactDOM.render(
-  <div>{Hello.getName()}</div>,
+  <div>hello</div>,
   document.querySelector('#root')
 );
