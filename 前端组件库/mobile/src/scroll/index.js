0124fd59981406c8e3413b050dfa011156053763
XingGuoZM <18969176324@163.com>
2021-03-02 17:35:13 +0800

    better-scroll

--- 前端组件库/mobile/src/scroll/index.js
+++ 前端组件库/mobile/src/scroll/index.js
@@ -17,14 +17,28 @@ export default function ScrollList() {
   })
 
   return <section className='wrapper'>
-    <ul className='content'>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-    </ul>
+    <div className='content'>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div>7</div>
+      <div>8</div>
+      <div>9</div>
+      <div>10</div>
+      <div>11</div>
+      <div>12</div>
+      <div>13</div>
+      <div>14</div>
+      <div>15</div>
+      <div>16</div>
+      <div>17</div>
+      <div>18</div>
+      <div>19</div>
+      <div>20</div>
+    </div>
   </section>
 }
 
