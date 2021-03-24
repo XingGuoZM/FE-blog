6fa0d65b5abf947ad5c3e59a2e595b6410efcf1e
XingGuoZM <18969176324@163.com>
2021-03-24 10:03:25 +0800

    http请求超时、重试

---
+++ code/js/http.js
@@ -0,0 +1,23 @@
+// http请求重试
+async function retry(fn, delay, count) {
+  try {
+    await fn();
+  } catch (e) {
+    if (count > 0) {
+      count--;
+      setTimeout(() => retry(fn, delay, count), delay);
+    }
+  }
+}
+// http请求并发
+function limit() {
+
+}
+// http请求超时
+function timeout(fn, ms) {
+  const mainPromise = fn();
+  const timeoutPromise = new Promise((resolve, reject) => {
+    setTimeout(() => reject('timeout...'), ms);
+  });
+  return Promise.race([mainPromise, timeoutPromise]);
+}
