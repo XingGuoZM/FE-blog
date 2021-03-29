918636da21052567c8ecc08ab40f72ffeff710de
XingGuoZM <18969176324@163.com>
2021-03-29 21:23:29 +0800

    http

--- code/js/http.js
+++ code/js/http.js
@@ -10,6 +10,38 @@ async function retry(fn, delay, count) {
   }
 }
 // http请求并发
+class Scheduler {
+  constructor() {
+    this.waitTasks = [];
+    this.runningTasks = [];
+    this.resolve = null;
+  }
+  add(task) {
+    return new Promise((resolve, reject) => {
+      task.resolve = resolve;
+      if (this.runningTasks.length < 2) {
+        this.run(task);
+      } else {
+        this.waitTasks.push(task);
+      }
+    });
+  }
+  run(task) {
+    this.runningTasks.push(task);
+    task().then(() => {
+      task.resolve();
+      this.remove(task);
+      if (this.waitTasks.length > 0) {
+        this.run(this.waitTasks.shift());
+      }
+    })
+  }
+  remove(task) {
+    let index = this.runningTasks.indexOf(task);
+    this.runningTasks.splice(index, 1);
+  }
+  //补全代码
+}
 function concurrent(urls, max, callback) {
 
 }
