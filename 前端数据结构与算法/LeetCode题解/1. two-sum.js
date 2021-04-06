119dfef18571bbe4c2b6a9503a5e846bd1590d7b
XingGuoZM <18969176324@163.com>
2021-04-06 10:49:05 +0800

    leetcode

--- 前端数据结构与算法/LeetCode题解/1. two-sum.js
+++ 前端数据结构与算法/LeetCode题解/1. two-sum.js
@@ -1,8 +1,11 @@
 
 /**
  * 
-给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
-你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+链接：https://leetcode-cn.com/problems/two-sum
+
+给定一个整数数组nums和一个整数目标值target，请你在该数组中找出和为目标值的那两个整数，并返回它们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+你可以按任意顺序返回答案。
 
 示例:
 给定 nums = [2, 7, 11, 15], target = 9
@@ -16,6 +19,6 @@
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = function(nums, target) {
+const twoSum = function (nums, target) {
 
 };
