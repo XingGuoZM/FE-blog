a75f164a81463200f4515adb52f3e19922584746
XingGuoZM <18969176324@163.com>
2021-04-13 09:49:50 +0800

    消消乐

---
+++ 前端小游戏/crush-saga/constant.js
@@ -0,0 +1,15 @@
+const FRUITS = {
+  0: '🍉',
+  1: '🍌',
+  2: '🍎',
+  3: '🥭',
+  4: '🍊',
+  5: '🍓',
+  6: '🍑',
+  7: '🍐',
+  8: '🍍',
+  9: '🍇',
+}
+
+const canvas = document.querySelector('#game');
+const ctx = canvas.getContext('2d');
--- 前端小游戏/crush-saga/index.js
+++ 前端小游戏/crush-saga/index.js
@@ -1,8 +1,6 @@
 
-const canvas = document.querySelector('#game');
-const ctx = canvas.getContext('2d');
-const FRUITS=['🍉','🍌','🍎','🥭','🍊','🍓','🍑','🍐','🍍','🍇'];
-let cells=[];
+
+let cells = [];
 
 // 绘制棋盘格
 function drawCheckerboard() {
@@ -11,7 +9,7 @@ function drawCheckerboard() {
   ctx.fillRect(0, 0, 500, 500);
   for (let i = 0; i <= 10; i++) {
     ctx.moveTo(5, 50 * i);
-    ctx.lineTo(505, 50 * i );
+    ctx.lineTo(505, 50 * i);
     ctx.moveTo(50 * i, 5);
     ctx.lineTo(50 * i, 505);
     ctx.font = "10px serif";
@@ -19,69 +17,73 @@ function drawCheckerboard() {
   ctx.stroke();
 }
 // 绘制水果icon
-function drawFruits(){
+function drawFruits() {
   ctx.font = "35px serif";
-  for(let i=0;i<10;i++){
-    for(let j=0;j<10;j++){
-      ctx.fillText(randomFruit(i), 12+50*i, 40+50*j);
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      ctx.fillText(randomFruit(i), 12 + 50 * i, 40 + 50 * j);
     }
   }
 }
 
-function randomFruit(i){
-  return FRUITS[Math.floor(Math.random(i)*10)]
+function randomFruit(i) {
+  return FRUITS[Math.floor(Math.random(i) * 10)]
 }
-function init(){
+function init() {
   drawCheckerboard();
   drawFruits();
 
 }
-function initCells(){
-    for(let i=0;i<10;i++){
-    cells[i]=[];
-    for(let j=0;j<10;j++){
-      cells[i][j]=0;
+function initCells() {
+  for (let i = 0; i < 10; i++) {
+    cells[i] = [];
+    for (let j = 0; j < 10; j++) {
+      cells[i][j] = 0;
     }
   }
 }
-function clean(){
-  ctx.clearRect(0,0,canvas.width, canvas.height);
+function clean() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
-function gameLoop(){
+function gameLoop() {
+
+}
+
+function eliminate() {
 
 }
 // 点击选中
-function getTarget(x,y){
+function getTarget(x, y) {
 
-    let cx=0;
-    let cy=0;
-    for(let i=0;i<10;i++){
-      if(x<51*i+50){
-        cx=i;
-        break;
-      }
+  let cx = 0;
+  let cy = 0;
+  for (let i = 0; i < 10; i++) {
+    if (x < 51 * i + 50) {
+      cx = i;
+      break;
     }
-    for(let i=0;i<10;i++){
-      if(y<51*i+50){
-        cy=i;
-        break;
-      }
+  }
+  for (let i = 0; i < 10; i++) {
+    if (y < 51 * i + 50) {
+      cy = i;
+      break;
     }
-    console.log(cells[cx][cy],cx,cy)
-    if(cells[cx][cy]===0){
-      cells[cx][cy]=1;
-      ctx.fillStyle = 'rgba(0,255,255,.5)';
-      ctx.fillRect(cx*50, cy*50, 50, 50);
-    }else if(cells[cx][cy]===1){
-      cells[cx][cy]=0;
-    }
+  }
+  console.log(cells[cx][cy], cx, cy)
+  if (cells[cx][cy] === 0) {
+    cells[cx][cy] = 1;
+    ctx.fillStyle = 'rgba(0,255,255,.5)';
+    ctx.fillRect(cx * 50, cy * 50, 50, 50);
+  } else if (cells[cx][cy] === 1) {
+    cells[cx][cy] = 0;
+  }
 }
 
 init();
 initCells();
-canvas.addEventListener('click',(e)=>{
+canvas.addEventListener('click', (e) => {
   // clean();
   // init();
-  
-  getTarget(e.clientX,e.clientY);
+
+  getTarget(e.clientX, e.clientY);
 });
