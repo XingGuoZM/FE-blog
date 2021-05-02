5a85f7b77878c4d9491ba3e0ae26cb18f9d75cc2
XingGuoZM <18969176324@163.com>
2021-05-02 23:09:56 +0800

    modal

--- 前端组件库/mobile/src/App.js
+++ 前端组件库/mobile/src/App.js
@@ -6,8 +6,8 @@ import {ModalManager, Tab, ScrollList, DragDemo } from './components';
 import { tabProps } from './config';
 function App() {
   viewport();
-  // const dom = <div>hello</div>
-  const mm = ModalManager('hello');
+  const dom = <div style={{backgroundColor:'#fff'}}>hello</div>
+  const mm = ModalManager(dom);
   // Modal.regist({key:'modal1'})
   return (
     <div className="App">
