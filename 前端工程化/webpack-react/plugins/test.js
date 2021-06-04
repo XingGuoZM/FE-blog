c3d8260edd1e037eb3abb59221fe6dd25fd7a7f4
XingGuoZM <18969176324@163.com>
2021-06-04 14:17:30 +0800

    webpack compilation

--- 前端工程化/webpack-react/plugins/test.js
+++ 前端工程化/webpack-react/plugins/test.js
@@ -1,6 +1,7 @@
 /**
  *  手写一个plugin示例
  */
+const fs = require('fs')
 const pluginName = 'consoleLogOnBuildWebpackPlugin';
 
 class MyPlugin {
@@ -8,15 +9,20 @@ class MyPlugin {
     compiler.hooks.compile.tap(pluginName, params => {
       console.log('webpack 构建过程开始');
     });
+    let modules = [];
     compiler.hooks.emit.tapAsync(pluginName, (compilation, cb) => {
       compilation.chunks.forEach(chunk => {
         // chunk包含多个模块，通过chunk.modulesIterable可以遍历模块列表      
         for (const module of chunk.modulesIterable) {
           // module包含多个依赖，通过module.dependencies进行遍历      	
+
           module.dependencies.forEach(dependency => {
-            console.log(dependency);
+            // console.log(dependency);
+            modules.push(dependency);
           });
+          // 
         }
+        fs.writeFileSync('./plugins/dependency.json', JSON.stringify(modules));
         cb();
       });
     })
