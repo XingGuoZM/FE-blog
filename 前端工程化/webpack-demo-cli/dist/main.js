e6f7d8b289ca036c3fd5fd63503f555dc5506f5a
XingGuoZM <18969176324@163.com>
2021-06-17 11:07:44 +0800

    前端工程化

--- 前端工程化/webpack-demo-cli/dist/main.js
+++ 前端工程化/webpack-demo-cli/dist/main.js
@@ -16,9 +16,10 @@
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _log__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./log */ \"./src/log.js\");\n\n\n(0,_log__WEBPACK_IMPORTED_MODULE_0__.default)('hello webpack');\n\n//# sourceURL=webpack:///./src/index.js?");
+        eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _log__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./log */ \"./src/log.js\");\n\n\n(0,_log__WEBPACK_IMPORTED_MODULE_0__.default)('hello webpack');\n\n//# sourceURL=webpack:///./src/index.js?");
 
-/***/ }),
+        /***/
+      }),
 
 /***/ "./src/log.js":
 /*!********************!*
@@ -26,68 +27,145 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _log
   \********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => __WEBPACK_DEFAULT_EXPORT__\n/* harmony export */ });\nfunction log(msg) {\n  console.log(msg);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (log);\n\n//# sourceURL=webpack:///./src/log.js?");
+        eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => __WEBPACK_DEFAULT_EXPORT__\n/* harmony export */ });\nfunction log(msg) {\n  console.log(msg);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (log);\n\n//# sourceURL=webpack:///./src/log.js?");
 
-/***/ })
+        /***/
+      })
 
-/******/ 	});
+    /******/
+  });
 /************************************************************************/
 /******/ 	// The module cache
 /******/ 	var __webpack_module_cache__ = {};
-/******/ 	
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/ 		// Check if module is in cache
-/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 		if (__webpack_module_cache__[moduleId]) {
 /******/ 			return __webpack_module_cache__[moduleId].exports;
-/******/ 		}
+      /******/
+    }
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
 /******/ 			// no module.id needed
 /******/ 			// no module.loaded needed
 /******/ 			exports: {}
-/******/ 		};
-/******/ 	
+      /******/
+    };
+/******/
 /******/ 		// Execute the module function
 /******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
-/******/ 	}
-/******/ 	
+    /******/
+  }
+/******/
 /************************************************************************/
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
 /******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 			for (var key in definition) {
+/******/ 				if (__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
 /******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
+          /******/
+        }
+        /******/
+      }
+      /******/
+    };
+    /******/
+  })();
+/******/
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
-/******/ 	})();
-/******/ 	
+    /******/
+  })();
+/******/
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
 /******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
 /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
+        /******/
+      }
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
+      /******/
+    };
+    /******/
+  })();
+/******/
 /************************************************************************/
 /******/ 	// startup
 /******/ 	// Load entry module
 /******/ 	__webpack_require__("./src/index.js");
-/******/ 	// This entry module used 'exports' so it can't be inlined
-/******/ })()
-;
+  /******/ 	// This entry module used 'exports' so it can't be inlined
+  /******/
+})()
+  ;
+
+
+
+
+
+__webpack_require__.r(__webpack_exports__);
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => __WEBPACK_DEFAULT_EXPORT__
+});
+function log(msg) {
+  console.log(msg);
+}
+const __WEBPACK_DEFAULT_EXPORT__ = (log);
+
+
+
+
+
+(() => {
+  "use strict";
+  var __webpack_modules__ = ({
+    "./src/index.js":
+      ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+        eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _log__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./log */ \"./src/log.js\");\n\n\n(0,_log__WEBPACK_IMPORTED_MODULE_0__.default)('hello webpack');\n\n//# sourceURL=webpack:///./src/index.js?");
+      }),
+    "./src/log.js":
+      ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+        eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => __WEBPACK_DEFAULT_EXPORT__\n/* harmony export */ });\nfunction log(msg) {\n  console.log(msg);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (log);\n\n//# sourceURL=webpack:///./src/log.js?");
+      })
+  });
+  var __webpack_module_cache__ = {};
+  function __webpack_require__(moduleId) {
+    if (__webpack_module_cache__[moduleId]) {
+      return __webpack_module_cache__[moduleId].exports;
+    }
+    var module = __webpack_module_cache__[moduleId] = {
+      exports: {}
+    };
+    __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+    return module.exports;
+  }
+  (() => {
+    __webpack_require__.d = (exports, definition) => {
+      for (var key in definition) {
+        if (__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+          Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+        }
+      }
+    };
+  })();
+  (() => {
+    __webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+  })();
+  (() => {
+    __webpack_require__.r = (exports) => {
+      if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+        Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+      }
+      Object.defineProperty(exports, '__esModule', { value: true });
+    };
+  })();
+  __webpack_require__("./src/index.js");
+})();
