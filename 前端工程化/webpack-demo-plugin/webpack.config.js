843f14baa2ea258cfe4a4bdfc9b5162fa42c02ab
XingGuoZM <18969176324@163.com>
2021-06-27 18:50:36 +0800

    webpack-plugin

--- 前端工程化/webpack-demo-plugin/webpack.config.js
+++ 前端工程化/webpack-demo-plugin/webpack.config.js
@@ -1,5 +1,13 @@
 
-const testWebpackPlugin = require('./plugin')
+// const testWebpackPlugin = require('./plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
+  entry: './src/index',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
 
 }
