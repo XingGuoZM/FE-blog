a6a1f36ffdcbe9629f92a997e37d2c290e61eb10
XingGuoZM <18969176324@163.com>
2021-06-30 17:50:34 +0800

    aboutMe

--- 前端工程化/webpack-demo-module/src/es-module/index.mjs
+++ 前端工程化/webpack-demo-module/src/es-module/index.mjs
@@ -1,6 +1,7 @@
 
-import util from './add.mjs';
-console.log(util);
+// import util from './add.mjs';
+// console.log(util);
 // const res = util.add(1, 2);
 // console.log(res);
 
+console.log(Symbol.toStringTag);
