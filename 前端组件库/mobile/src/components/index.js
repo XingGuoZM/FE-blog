54298af6e8d87801e2aaf283de2cd3f23f421dff
XingGuoZM <18969176324@163.com>
2021-08-06 19:15:10 +0800

    countdown

--- 前端组件库/mobile/src/components/index.js
+++ 前端组件库/mobile/src/components/index.js
@@ -4,11 +4,13 @@ import ModalManager from './modal/modalManager';
 import ScrollList from './scroll';
 import DragDemo from './drag';
 import Marquee from './marquee';
+import CountDown from './countdown';
 
 export {
   ModalManager,
   Tab,
   ScrollList,
   DragDemo,
-  Marquee
+  Marquee,
+  CountDown
 }
