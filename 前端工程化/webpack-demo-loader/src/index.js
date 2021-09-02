469cc3fb5392bfb77f9c45559d3dfd5d8b1c019e
XingGuoZM <18969176324@163.com>
2021-09-02 10:16:56 +0800

    babel-loader custom

--- 前端工程化/webpack-demo-loader/src/index.js
+++ 前端工程化/webpack-demo-loader/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Hello from './hello.jsx';
 
 ReactDOM.render(
-  'Hello, world!',
+  <Hello />,
   document.getElementById('root')
 );
--- 前端工程化/webpack-demo-loader/webpack.config.js
+++ 前端工程化/webpack-demo-loader/webpack.config.js
@@ -4,6 +4,18 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' }
+      },
+      {
+        loader: path.join(__dirname, 'loaders/custom-babel-loader.js')
+      }
+    ]
+  },
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
