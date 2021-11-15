6bb339d80232752c9dc2794b0e568d61fee2e584
XingGuoZM <18969176324@163.com>
2021-11-15 16:59:15 +0800

    babel parse generate traverse

--- 前端工程化/babel-demo/index.js
+++ 前端工程化/babel-demo/index.js
@@ -1,5 +1,22 @@
-const babelParser = require("@babel/parser");
+const { parse } = require("@babel/parser");
+const generate = require("@babel/generator");
+const traverse = require("@babel/traverse");
+const fs = require('fs');
 // parser:解析
-babelParser.parse("()=>{console.log(123)}", {});
+// babelParser.parse("()=>{console.log(123)}", {});
 // tranform:转换
 // generate:生成
+const code = 'const fn=(n)=>n*n';
+const ast = parse(code);
+fs.writeFileSync('./path.json', JSON.stringify(ast));
+traverse.default(ast, {
+  enter(path) {
+    // console.log(path.isIdentifier({ name: "n" }));
+    if (path.isIdentifier({ name: "n" })) {
+      path.node.name = 'x';
+    }
+  }
+});
+const output = generate.default(ast, {}, code);
+console.log(output);
+// console.log(output)
