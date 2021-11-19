465a0105cfdbf4f4f0afd9382617632d834ed36e
XingGuoZM <18969176324@163.com>
2021-11-19 15:28:28 +0800

    html2Object

--- code/js/html2Object.js
+++ code/js/html2Object.js
@@ -1,5 +1,5 @@
-const htmlStr = `<div id="main" data-x="hello">Hello<span id="sub" /></div>`;
-
+let htmlStr = `<div id="main" data-x="hello">Hello<span id="sub" /></div>`;
+// let htmlStr = `<div id="main" data-x="hello"/>`;
 // const result = {
 //   tag: "div",
 //   selfClose: false,
@@ -18,8 +18,40 @@ const htmlStr = `<div id="main" data-x="hello">Hello<span id="sub" /></div>`;
 //     }
 //   ]
 // }
+const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
+const dynamicArgAttribute = /^\s*((?:v-[\w-]+:|@|:|#)\[[^=]+?\][^\s"'<>\/=]*)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
+const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`
+const qnameCapture = `((?:${ncname}\\:)?${ncname})`
+const startTagOpen = new RegExp(`^<${qnameCapture}`)
+const startTagClose = /^\s*(\/?)>/
+const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`)
 
+const advance = (n) => {
+  htmlStr = htmlStr.substring(n);
+}
+const parseStartTag = () => {
+  const start = htmlStr.match(startTagOpen);
+  if (start) {
+    const match = {
+      tagName: start[1],
+      attrs: []
+    };
+    advance(start[0].length);
+    let attr, end;
+    while (!(end = htmlStr.match(startTagClose)) && (attr = htmlStr.match(attribute))) {
+      advance(attr[0].length);
+      match.attrs.push({ key: attr[1], value: attr[3] });
+    }
+    if (end) {
+      advance(end[0].length);
+      return match;
+    }
+  }
+}
 
-
+parseStartTag();
+// console.log(htmlStr);
+const res = '</span>123'.match(endTag);
+console.log(res);
 
 
