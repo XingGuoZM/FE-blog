8d7ffebf1b531b028fe884c16996f85f41ad7871
XingGuoZM <18969176324@163.com>
2021-11-23 15:59:24 +0800

    html2Object

--- code/js/html2Object.js
+++ code/js/html2Object.js
@@ -1,81 +1,80 @@
+/**
+ * 输入：'<div id="main" data-x="hello">Hello<span id="sub" /></div>'
+ * 输出：
+{
+  tag: "div",
+  selfClose: false,
+  attributes: {
+    "id": "main",
+    "data-x": "hello"
+  },
+  text: "Hello",
+  children: [
+    {
+      tag: "span",
+      selfClose: true,
+      attributes: {
+        "id": "sub"
+      }
+    }
+  ]
+}
+ * 
+ */
+
 let htmlStr = `<div id="main" data-x="hello">Hello<span id="sub" /></div>`;
-// let htmlStr = `<div id="main" data-x="hello"/>`;
-// const result = {
-//   tag: "div",
-//   selfClose: false,
-//   attributes: {
-//     "id": "main",
-//     "data-x": "hello"
-//   },
-//   text: "Hello",
-//   children: [
-//     {
-//       tag: "span",
-//       selfClose: true,
-//       attributes: {
-//         "id": "sub"
-//       }
-//     }
-//   ]
-// }
+
 const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
 const dynamicArgAttribute = /^\s*((?:v-[\w-]+:|@|:|#)\[[^=]+?\][^\s"'<>\/=]*)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
 const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`
 const qnameCapture = `((?:${ncname}\\:)?${ncname})`
 const startTagOpen = new RegExp(`^<${qnameCapture}`)
 const startTagClose = /^\s*(\/?)>/
-const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`)
+const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`);
 let stack = [];
 let root, currentParent;
-const createAstElement = (tag, attr) => {
-  return {
-    type: 1,
+const start = (tag, attributes, selfClose, text) => {
+  const element = {
     tag,
-    attr,
+    attributes,
+    selfClose,
+    text,
     children: [],
-    parent: null,
+    parent: null
   }
-}
-const start = (tagName, attr) => {
-  const element = createAstElement(tagName, attr);
   if (!root) root = element;
   currentParent = element;
   stack.push(element);
 }
-const end = (tagName) => {
+const end = () => {
   const element = stack[stack.length - 1];
   stack.length--;
   currentParent = stack[stack.length - 1];
   if (currentParent) {
     element.parent = currentParent;
-    currentParent.children = element;
+    currentParent.children.push(element);
   }
-}
-const chars = (text) => {
-  currentParent.children.push({
-    type: 3,
-    text,
-  })
-}
-const advance = (n) => {
-  htmlStr = htmlStr.substring(n);
 }
 const parseStartTag = () => {
   const start = htmlStr.match(startTagOpen);
   if (start) {
     const match = {
       tagName: start[1],
-      attrs: [],
-      inner: ''
+      attrs: {},
+      selfClose: false,
+      text: '',
     };
-    advance(start[0].length);
+    htmlStr = htmlStr.substring(start[0].length);
     let attr, end;
     while (!(end = htmlStr.match(startTagClose)) && (attr = htmlStr.match(attribute))) {
-      advance(attr[0].length);
-      match.attrs.push({ key: attr[1], value: attr[3] });
+      htmlStr = htmlStr.substring(attr[0].length);
+      match.attrs[attr[1]] = attr[3];
     }
     if (end) {
-      advance(end[0].length);
+      if (end[0].trim() === '/>') {
+        match.selfClose = true;
+      }
+      htmlStr = htmlStr.substring(end[0].length);
       return match;
     }
   }
@@ -86,23 +85,21 @@ while (htmlStr) {
   if (endIndex == 0) {
     const startMatch = parseStartTag();
     if (startMatch) {
-      start(startMatch.tagName, startMatch.attrs);
+      start(startMatch.tagName, startMatch.attrs, startMatch.selfClose);
       continue;
     }
     const endMatch = htmlStr.match(endTag);
     if (endMatch) {
-      advance(endMatch[0].length);
-      end(endMatch[1]);
+      htmlStr = htmlStr.substring(endMatch[0].length);
+      end();
     }
   } else if (endIndex > 0) {
     text = htmlStr.substring(0, endIndex);
-    chars(text);
+    currentParent.text = text;
   }
   if (text) {
-    advance(text.length);
+    htmlStr = htmlStr.substring(text.length);
   }
 }
 
-console.log(root);
-
-
+console.log(root)
