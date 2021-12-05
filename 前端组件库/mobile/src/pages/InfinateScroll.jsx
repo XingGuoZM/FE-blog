66685e53ea4d3258e4767dae728062b0cf33c46d
XingGuoZM <18969176324@163.com>
2021-12-05 22:00:25 +0800

    infinateScroll

--- 前端组件库/mobile/src/pages/InfinateScroll.jsx
+++ 前端组件库/mobile/src/pages/InfinateScroll.jsx
@@ -1,6 +1,12 @@
 import { useEffect, useState, useRef } from 'react';
 import './infinateScroll.css';
-
+/**
+ * 构造一个滑动窗口，
+ * 该窗口包含两页数据dom节点，
+ * 每次滑动到第二页最底部的时候，
+ * 使用transform:translateY来瞬间移动列表至第一页的底部，
+ * 同时数据必须同步更新，来达到列表在更新滚动的效果
+ */
 const defaultPageSize = 10;
 let num = 1;
 const queryData = (pageNum = 1, pageSize = defaultPageSize) => {
@@ -10,15 +16,20 @@ const queryData = (pageNum = 1, pageSize = defaultPageSize) => {
       numbers.push(i);
     }
   }
-  return new Promise(resolve => setTimeout(() => resolve(numbers), 1000));
+  return new Promise(resolve => setTimeout(() => resolve(numbers), 5000));
 }
-
+const casheData=[
+  [1,2,3,4,5,6,7,8,9,10],
+  [6,7,8,9,10,11,12,13,14,15],
+  [11,12,13,14,15, 16,17,18,19,20],
+];
 function InfinateScroll() {
   const wrapRef = useRef();
   const listRef = useRef();
   const endItemRef = useRef();
-  const [list, setList] = useState([]);
+  const [list, setList] = useState([1,2,3,4,5,6,7,8,9]);
   const [isEnd, setIsEnd] = useState(false);
+  const[y,setY]=useState(0);
   const getData = async (pageNum) => {
     const res = await queryData(pageNum);
     if (res.length < defaultPageSize) {
@@ -27,21 +38,25 @@ function InfinateScroll() {
     setList((value) => value.concat(res));
   }
   useEffect(() => {
-    getData();
-    wrapRef.current.addEventListener('scroll', () => {
+    // getData();
+    const itemHeight = listRef.current?.children[0].offsetHeight;
+    wrapRef.current.addEventListener('scroll', (e) => {
       const endRect = endItemRef.current.getBoundingClientRect();
       const wrapRect = wrapRef.current.getBoundingClientRect();
       if (endRect.top + endRect.height <= wrapRect.height) {
-        getData(++num);
+        // getData(++num);
+        ++num;
+        setY((value)=>value+itemHeight*5);
       }
     });
-  }, []);
+  }, [listRef.current]);
 
   return <div className='wrap' ref={wrapRef}>
-    <div className='list' ref={listRef}>
-      {list?.map((item, index) => <div className='item' key={index}>{item}</div>)}
+    <div className='list' ref={listRef} style={{transform:`translateY(${y}px)`}}>
+      {list.map((item, index) => <div className='item' key={index}>{casheData[num-1][item-1]}</div>)}
       <div className='endItem' ref={endItemRef}>{isEnd ? '到底了~' : '加载中...'}</div>
     </div>
+
   </div>
 }
 
