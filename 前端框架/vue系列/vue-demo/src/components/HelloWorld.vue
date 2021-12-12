114bf70e19f22c22f5b09cb40d19e07c455dc34e
XingGuoZM <18969176324@163.com>
2021-12-12 15:40:10 +0800

    vue

---
+++ 前端框架/vue系列/vue-demo/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+{{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props:{
+    msg:{type:String,default:'hello'}
+  }
+}
+</script>
