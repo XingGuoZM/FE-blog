fc85708c5dbaa02aae44c8054a697c72e34bf842
XingGuoZM <18969176324@163.com>
2022-02-09 15:47:59 +0800

    setTimeout requestAnimationFrame动画

--- code/js/microTasks.js
+++ code/js/microTasks.js
@@ -1,10 +1,21 @@
-new Promise((resolve,reject)=>{
-  reject();
-  console.log(1)
-}).then(res=>{
-  console.log(2)
-},error=>{
-  console.log(3)
-}).then(res=>{
-  console.log(4)
-})
+// new Promise((resolve, reject) => {
+//   reject();
+//   console.log(1)
+// }).then(res => {
+//   console.log(2)
+// }, error => {
+//   console.log(3)
+// }).then(res => {
+//   console.log(4)
+// })
+console.log(1)
+new Promise(resolve => resolve()).then(() => console.log(3));
+
+Promise.resolve().then(() => console.log(2));
+
+setTimeout(() => { console.log(4) }, 500);
+const prev = +new Date();
+while (+new Date - prev < 1000) {
+
+}
+console.log(5);
