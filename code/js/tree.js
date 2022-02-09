7396c7fce9bb09efe5dedd5b18f6df205daaa38d
XingGuoZM <18969176324@163.com>
2022-02-09 17:39:32 +0800

    树 层序遍历

--- code/js/tree.js
+++ code/js/tree.js
@@ -4,9 +4,9 @@ function Node(data) {
   this.right = null;
 }
 
-  //   1
-  // 2    3
-  //   4 5 6
+//   1
+// 2    3
+//   4 5 6
 function buildTree() {
   let root = new Node(1);
   root.left = new Node(2);
@@ -18,15 +18,51 @@ function buildTree() {
   return root;
 }
 
-function traverse(root,res,count){
-  if(!root) return ;
-  if(!res[count]) res[count] = [];
+function traverse(root, res, count) {
+  if (!root) return;
+  if (!res[count]) res[count] = [];
   res[count].push(root.data);
-  traverse(root.left,res,count+1);
-  traverse(root.right,res,count+1);
+  traverse(root.left, res, count + 1);
+  traverse(root.right, res, count + 1);
+}
+
+// 层序遍历(递归)
+function levelTraverse1(root) {
+  const res = [];
+  function levelOrder(node, level) {
+    if (!node) return null;
+    res[level] = res[level] || [];
+    res[level].push(node.data);
+    levelOrder(node.left, level + 1);
+    levelOrder(node.right, level + 1);
+  }
+  levelOrder(root, 0);
+  return res;
+}
+// 层序遍历(队列)
+function levelTraverse2(root) {
+  if (!root) return [];
+  const res = [];
+  let level = 0;
+  const queue = [root];
+  while (queue.length > 0) {
+    res.push([]);
+    const len = queue.length;
+    for (let i = 0; i < len; i++) {
+      const node = queue.shift();
+      res[level].push(node.data);
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+
+    }
+    ++level;
+  }
+  return res;
 }
 
 let res = {};
 let root = buildTree();
-traverse(root,res,0);
-console.log(res);
+const ans = levelTraverse2(root);
+console.log(ans);
+// traverse(root,res,0);
+// console.log(res);
