9b18ef3a75551fda2b48db20827b78a13fb188fd
XingGuoZM <18969176324@163.com>
2022-02-14 18:36:38 +0800

    前端面试

--- 前端框架/react系列/react-hooks-demo/src/App.js
+++ 前端框架/react系列/react-hooks-demo/src/App.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 // import {FunctionPage} from './pages/FunctionPage';
 // import {ClassPage} from './pages/ClassPage';
 // import Message from './pages/Message';
@@ -7,16 +7,17 @@ import React,{useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  const [count,setCount] = useState(1);
+  const [count, setCount] = useState(1);
   // const [isEmpty,setIsEmpty] = useState(true);
   // useEffect(()=>{
   //   setTimeout(()=>setIsEmpty(false),2000);
   // },[])
+  const add = () => { setCount(v => v + v) }
   return (
     <div className="App">
-        <button onClick={()=>setCount(value=>value+value)}>change count</button>
-        <div>count:{count}</div>
-        {/* <ClassPage count={count}/>
+      <button onClick={add}>change count</button>
+      <div>count:{count}</div>
+      {/* <ClassPage count={count}/>
         <FunctionPage count={count}/>
         <Message/>
         <Search/>
